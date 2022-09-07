687e324cdd25fa9c9f586ced3ce89377322597db
Shamyra Messias <shamyramesssias2811@gmail.com>
2022-09-06 21:51:12 -0300

    estou finalizando a atividade

---
+++ clients/MultApps.Client.Windows/Funcionalidades/frmPesoIdeal.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace MultApps.Client.Windows.Funcionalidades
+{
+    partial class frmPesoIdeal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 51);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Feminino";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 76);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Masculino";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(27, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sexo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(196, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Altura:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(93, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Peso Ideal";
+            // 
+            // frmPesoIdeal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 175);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "frmPesoIdeal";
+            this.Text = "frmPesoIdeal";
+            this.Load += new System.EventHandler(this.frmPesoIdeal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ clients/MultApps.Client.Windows/Principal.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MultApps.Client.Windows
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.btnAbrirFormMatriculaAluno = new System.Windows.Forms.Button();
+            this.btnAbrirFormReajusteSalarial = new System.Windows.Forms.Button();
+            this.btnAbrirFormPesoIdeal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbrirFormMatriculaAluno
+            // 
+            this.btnAbrirFormMatriculaAluno.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAbrirFormMatriculaAluno.Image = ((System.Drawing.Image)(resources.GetObject("btnAbrirFormMatriculaAluno.Image")));
+            this.btnAbrirFormMatriculaAluno.Location = new System.Drawing.Point(181, 50);
+            this.btnAbrirFormMatriculaAluno.Name = "btnAbrirFormMatriculaAluno";
+            this.btnAbrirFormMatriculaAluno.Size = new System.Drawing.Size(154, 102);
+            this.btnAbrirFormMatriculaAluno.TabIndex = 1;
+            this.btnAbrirFormMatriculaAluno.Text = "Matricula de Aluno";
+            this.btnAbrirFormMatriculaAluno.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAbrirFormMatriculaAluno.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAbrirFormMatriculaAluno.UseVisualStyleBackColor = true;
+            this.btnAbrirFormMatriculaAluno.Click += new System.EventHandler(this.btnAbrirFormMatriculaAluno_Click);
+            // 
+            // btnAbrirFormReajusteSalarial
+            // 
+            this.btnAbrirFormReajusteSalarial.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAbrirFormReajusteSalarial.Image = ((System.Drawing.Image)(resources.GetObject("btnAbrirFormReajusteSalarial.Image")));
+            this.btnAbrirFormReajusteSalarial.Location = new System.Drawing.Point(364, 50);
+            this.btnAbrirFormReajusteSalarial.Name = "btnAbrirFormReajusteSalarial";
+            this.btnAbrirFormReajusteSalarial.Size = new System.Drawing.Size(147, 102);
+            this.btnAbrirFormReajusteSalarial.TabIndex = 2;
+            this.btnAbrirFormReajusteSalarial.Text = "Reajuste Salarial";
+            this.btnAbrirFormReajusteSalarial.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAbrirFormReajusteSalarial.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAbrirFormReajusteSalarial.UseVisualStyleBackColor = true;
+            this.btnAbrirFormReajusteSalarial.Click += new System.EventHandler(this.btnAbrirFormReajusteSalarial_Click);
+            // 
+            // btnAbrirFormPesoIdeal
+            // 
+            this.btnAbrirFormPesoIdeal.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAbrirFormPesoIdeal.Image = ((System.Drawing.Image)(resources.GetObject("btnAbrirFormPesoIdeal.Image")));
+            this.btnAbrirFormPesoIdeal.Location = new System.Drawing.Point(12, 50);
+            this.btnAbrirFormPesoIdeal.Name = "btnAbrirFormPesoIdeal";
+            this.btnAbrirFormPesoIdeal.Size = new System.Drawing.Size(143, 102);
+            this.btnAbrirFormPesoIdeal.TabIndex = 0;
+            this.btnAbrirFormPesoIdeal.Text = "Peso Ideal";
+            this.btnAbrirFormPesoIdeal.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAbrirFormPesoIdeal.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnAbrirFormPesoIdeal.UseVisualStyleBackColor = true;
+            this.btnAbrirFormPesoIdeal.Click += new System.EventHandler(this.btnAbrirFormPesoIdeal_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(523, 200);
+            this.Controls.Add(this.btnAbrirFormReajusteSalarial);
+            this.Controls.Add(this.btnAbrirFormMatriculaAluno);
+            this.Controls.Add(this.btnAbrirFormPesoIdeal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultApps";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button btnAbrirFormMatriculaAluno;
+        private Button btnAbrirFormReajusteSalarial;
+        private Button btnAbrirFormPesoIdeal;
+    }
+}
